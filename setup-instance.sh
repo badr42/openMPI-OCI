e40c6b3793270ded1f29cfbfc6cceceae9defb85
badr42 <badr42@gmail.com>
2022-11-20 00:37:50 +0300

    with sample code

--- setup-instance.sh
+++ setup-instance.sh
@@ -10,7 +10,8 @@ sudo apt-get upgrade -yq
 
 sudo apt-get install net-tools -yq
 sudo mkdir -p /home/ubuntu/Desktop/sharedfolder
-sudo apt-get install gcc openmpi-bin openmpi-common libopenmpi-dev libgtk2.0-dev -yq
+sudo chown ubuntu:ubuntu Desktop -R
+sudo apt-get install gcc openmpi-bin openmpi-common libopenmpi-dev libgtk2.0-dev vim -yq
 sudo wget https://www.open-mpi.org/software/ompi/v4.1/downloads/openmpi-4.1.1.tar.gz
 sudo tar -xvzf openmpi-4.1.1.tar.gz
 cd openmpi-4.1.1
